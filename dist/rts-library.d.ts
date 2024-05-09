cdc067e7c29586203a011c6f289f748d42d25572
kyawyenaing <kyawyenaing.dev@gmail.com>
2024-05-09 13:00:56 +0700

    build PRadio

--- dist/rts-library.d.ts
+++ dist/rts-library.d.ts
@@ -1,6 +1,7 @@
 import { ClassProp } from 'class-variance-authority/types';
 import { ComponentProps } from 'react';
 import { ForwardRefExoticComponent } from 'react';
+import { JSX as JSX_2 } from 'react/jsx-runtime';
 import { RefAttributes } from 'react';
 import { VariantProps } from 'class-variance-authority';
 
@@ -13,5 +14,14 @@ declare const buttonStyles: (props?: ({
 export declare const PButton: ForwardRefExoticComponent<Omit<PButtonProps, "ref"> & RefAttributes<HTMLButtonElement>>;
 
 declare type PButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
+
+export declare const PRadio: ({ variant, size, ...props }: PRadioProps) => JSX_2.Element;
+
+declare type PRadioProps = ComponentProps<"input"> & VariantProps<typeof radioStyles>;
+
+declare const radioStyles: (props?: ({
+    variant?: "solid" | "readonly" | null | undefined;
+    size?: "sm" | "md" | "lg" | null | undefined;
+} & ClassProp) | undefined) => string;
 
 export { }
